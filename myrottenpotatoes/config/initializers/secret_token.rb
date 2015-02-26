97e5954f1d50217efbdf5af750b14dfa7c63d3ee
Victor-Benitez <vbenite1@uncc.edu>
2015-02-25 20:00:49 -0400

    Initial Commit Test with slight index.html.haml error

---
+++ myrottenpotatoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '960b310c8f3ceea88f916ce4cee27b0dbce8243f4d056d467601c314bb57d0ef8a738ed50781409a4d5b359462f63ce187a90c703a8e7e8e301755e6bf674871'
